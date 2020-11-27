91b15642cb72d77e683dfbad686f04efb103c47b
Hayden MacDonald <hayden.macdonald.8778@gmail.com>
2020-11-27 16:01:48 +0000

    Initiate refactor on tournament functions

--- R/get_tournaments.R
+++ R/get_tournaments.R
@@ -50,108 +50,125 @@
 #'
 #' @export
 
-get_tournaments <- function(year = 2020, world_tour = TRUE) {
+get_tournaments <-
+  function(year = 2020,
+           world_tour = TRUE) {
+    ## Input errors
+    stopifnot(
+      is.numeric(year) |
+        is.null(year),
+      (nchar(trunc(abs(
+        year
+      ))) == 4 & year > 0) | is.null(year),
+      is.logical(world_tour)
+    )
 
-  stopifnot(is.numeric(year) | is.null(year), (nchar(trunc(abs(year))) == 4 & year > 0) | is.null(year), is.logical(world_tour))
+    # Results page on SquashInfo
+    t_url <- "http://www.squashinfo.com/results?start=1"
 
-  # Results page on SquashInfo
-  t_url <- "http://www.squashinfo.com/results?start=1"
+    # Empty tournaments object
+    tournaments <- c()
 
-  # Empty tournaments object
-  tournaments <- c()
+    # Set arbitrary variable to test for tournament result limit
+    results_limit <- 0
 
-  # Set arbitrary variable to test for tournament result limit
-  results_limit <- 0
+    while (!is.na(results_limit)) {
+      ## Verbose
+      message("Scraping ", t_url)
 
-  while (!is.na(results_limit)) {
+      # Check for Robots.txt
+      session <- suppressMessages(bow(t_url))
 
-    ## Verbose
-    message("Scraping ", t_url)
+      # Nod and scrape page politely
+      current_page <- nod(session, t_url) %>%
+        scrape(verbose = FALSE)
 
-    # Check for Robots.txt
-    session <- suppressMessages(bow(t_url))
+      ## Extract tournaments table
+      results <- current_page %>%
+        html_nodes("div.darkborder") %>%
+        html_nodes("table") %>%
+        .[[2]] %>%
+        html_table(header = TRUE) %>%
+        as.data.frame() %>%
+        clean_names() %>%
+        rename(league = x) %>%
+        mutate(date = dmy(date)) %>%
+        filter(!str_detect(name, pattern = "Premier League"))
 
-    # Nod and scrape page politely
-    current_page <- nod(session, t_url) %>%
-                          scrape(verbose = FALSE)
+      ## Filter out non-world tour tournaments
+      if (world_tour == TRUE) {
+        results <- results %>%
+          filter(str_detect(league, pattern = "World"))
 
-    ## Extract tournaments table
-    results <- current_page %>%
-                  html_nodes("div.darkborder") %>%
-                  html_nodes("table") %>%
-                  .[[2]] %>%
-                  html_table(header = TRUE) %>%
-                  as.data.frame() %>%
-                  clean_names() %>%
-                  rename(league = x) %>%
-                  mutate(date = dmy(date)) %>%
-                  filter(!str_detect(name, pattern = "Premier League"))
+      }
 
-    ## Filter out non-world tour tournaments
-    if (world_tour == TRUE) {
+      ## Bind results row-wise
+      tournaments <- rbind(tournaments, results)
 
-      results <- results %>%
-                    filter(str_detect(league, pattern = "World"))
+      ## If results begin to include previous year tournaments, break while loop, otherwise continue to next page
+      if ((year - 1) %in% year(tournaments$date)) {
+        results_limit <- NA_character_
+
+      } else {
+        results_limit <- 0
+
+        # Find url in "Next" button
+        t_url <- current_page %>%
+          html_nodes("a")
+
+        t_url <-
+          suppressWarnings(t_url[str_detect(t_url, "Next")]) %>%
+          html_attr("href")
+
+        ## Replace t_url with url in Next page button
+        t_url <- sprintf("http://www.squashinfo.com%s", t_url)
+
+      }
 
     }
 
-    ## Bind results row-wise
-    tournaments <- rbind(tournaments, results)
 
-    ## If results begin to include previous year tournaments, break while loop, otherwise continue to next page
-    if ((year - 1) %in% year(tournaments$date)) {
+    ## Clean tournament results
+    tournaments <- tournaments %>%
+      as_tibble() %>%
+      select(league, name, location, date) %>%
+      mutate(
+        league = if_else(league == "", NA_character_, league),
+        ## Replace empty strings with NAs
+        category = if_else(
+          str_detect(name, regex("\\(M\\)")),
+          "Men's",
+          if_else(str_detect(name, regex("\\(W\\)")), "Women's", NA_character_)
+        ),
+        ## Derive category from tournament name
+        name = str_replace_all(name, pattern = regex(" \\(M\\)"), ""),
+        name = str_replace_all(name, pattern = regex(" \\(W\\)"), ""),
+        city = str_extract(location, "(.*)(?=, )"),
+        ## extract city and country from location
+        country = if_else(
+          str_count(location, ",") == 2,
+          str_trim(str_remove(location, "([^,]+,[^,]+),."), side = "left"),
+          str_trim(str_extract(location, "(?<=, )(.*)"), side = "left")
+        )
+      ) %>%
+      filter(date >= ymd('2019-01-01')) %>%  ## filter out any tournaments occuring before 2019-01-01 (tournament data before this date is not available to regular members of SquashInfo)
+      arrange(desc(date)) %>%
+      select(league, category, name, date, city, country)
 
-      results_limit <- NA_character_
 
-    } else {
+    ## Filter final results by year input
+    if (year == 2020) {
+      tournaments <- tournaments %>%
+        filter(year(date) == 2020)
 
-      results_limit <- 0
 
-      # Find url in "Next" button
-      t_url <- current_page %>%
-                          html_nodes("a")
-
-      t_url <- suppressWarnings(t_url[str_detect(t_url, "Next")]) %>%
-                  html_attr("href")
-
-      ## Replace t_url with url in Next page button
-      t_url <- sprintf("http://www.squashinfo.com%s", t_url)
+    } else if (year == 2019) {
+      tournaments <- tournaments %>%
+        filter(year(date) == 2019)
 
     }
 
-  }
 
-
-  ## Clean tournament results
-  tournaments <- tournaments %>%
-                    as_tibble() %>%
-                    select(league, name, location, date) %>%
-                    mutate(league = if_else(league == "", NA_character_, league), ## Replace empty strings with NAs
-                           category = if_else(str_detect(name, regex("\\(M\\)")), "Men's", if_else(str_detect(name, regex("\\(W\\)")), "Women's", NA_character_)), ## Derive category from tournament name
-                           name = str_replace_all(name, pattern = regex(" \\(M\\)"), ""),
-                           name = str_replace_all(name, pattern = regex(" \\(W\\)"), ""),
-                           city = str_extract(location, "(.*)(?=, )"), ## extract city and country from location
-                           country = if_else(str_count(location, ",") == 2, str_trim(str_remove(location, "([^,]+,[^,]+),."), side = "left"), str_trim(str_extract(location, "(?<=, )(.*)"), side = "left"))) %>%
-                    filter(date >= ymd('2019-01-01')) %>%  ## filter out any tournaments occuring before 2019-01-01 (tournament data before this date is not available to regular members of SquashInfo)
-                    arrange(desc(date)) %>%
-                    select(league, category, name, date, city, country)
-
-
-  ## Filter final results by year input
-  if (year == 2020) {
-
-    tournaments <- tournaments %>%
-                        filter(year(date) == 2020)
-
-
-  } else if (year == 2019) {
-
-    tournaments <- tournaments %>%
-                        filter(year(date) == 2019)
+    return(tournaments)
 
   }
-
-
-  return(tournaments)
-
-}
