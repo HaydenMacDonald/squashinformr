95f71e64f0b16c5ee9a6715b68dece0b861f1184
Hayden MacDonald <hayden.macdonald.8778@gmail.com>
2021-04-14 14:41:59 +0100

    fix: switched tests to use :: syntax instead of library(squashinformr)

--- tests/testthat/test_get_tournament_games.R
+++ tests/testthat/test_get_tournament_games.R
@@ -1,5 +1,4 @@
 context("Tournament Games")
-library(squashinformr)
 
 test_that("test get_tournament_games for wrong input errors", {
 
@@ -7,19 +6,19 @@ test_that("test get_tournament_games for wrong input errors", {
   testthat::skip_on_cran()
 
   ## If integer provided to tournament
-  expect_error(get_tournament_games(1, year = 2020, world_tour = TRUE))
+  expect_error(squashinformr::get_tournament_games(1, year = 2020, world_tour = TRUE))
   ## If date provided to year
-  expect_error(get_tournament_games("Black Ball Open", year = "2020-01-01", world_tour = TRUE))
+  expect_error(squashinformr::get_tournament_games("Black Ball Open", year = "2020-01-01", world_tour = TRUE))
   ## If string provided to world_tour
-  expect_error(get_tournament_games("Black Ball Open", year = 2020, world_tour = "TRUE"))
+  expect_error(squashinformr::get_tournament_games("Black Ball Open", year = 2020, world_tour = "TRUE"))
   ## If tournament is "Premier League"
-  expect_error(get_tournament_games("Premier League", year = 2020, world_tour = TRUE))
+  expect_error(squashinformr::get_tournament_games("Premier League", year = 2020, world_tour = TRUE))
   ## If tournament is beyond available data
-  expect_error(get_tournament_games("Black Ball Open", year = 2018, world_tour = TRUE))
+  expect_error(squashinformr::get_tournament_games("Black Ball Open", year = 2018, world_tour = TRUE))
   ## If integer provided to year
-  expect_error(get_tournament_games(year = 20, world_tour = TRUE))
+  expect_error(squashinformr::get_tournament_games(year = 20, world_tour = TRUE))
   ## If negative integer provided to year
-  expect_error(get_tournament_games(year = -2020, world_tour = TRUE))
+  expect_error(squashinformr::get_tournament_games(year = -2020, world_tour = TRUE))
 
 })
 
@@ -29,21 +28,21 @@ test_that("test get_tournament_games for proper outputs", {
   testthat::skip_on_cran()
 
   ## Black Ball Open, with year
-  df <- get_tournament_games("Black Ball Open", year = 2021, world_tour = TRUE)
+  df <- squashinformr::get_tournament_games("Black Ball Open", year = 2021, world_tour = TRUE)
   expect_is(df, "data.frame")
   expect_is(df, "tbl")
   expect_is(sample(df$tournament_date, 1), "Date")
   expect_equal(year(sample(df$tournament_date, 1)), 2021)
 
   ## Black Ball Open, without year
-  df <- get_tournament_games("Black Ball Open", year = NULL, world_tour = TRUE)
+  df <- squashinformr::get_tournament_games("Black Ball Open", year = NULL, world_tour = TRUE)
   expect_is(df, "data.frame")
   expect_is(df, "tbl")
   expect_is(sample(df$tournament_date, 1), "Date")
   expect_equal(year(sample(df$tournament_date, 1)), 2021)
 
   ## All 2020 tournaments
-  df <- get_tournament_games(tournament = NULL, year = 2021, world_tour = FALSE)
+  df <- squashinformr::get_tournament_games(tournament = NULL, year = 2021, world_tour = FALSE)
   expect_is(df, "data.frame")
   expect_is(df, "tbl")
   expect_is(sample(df$tournament_date, 1), "Date")
--- tests/testthat/test_get_tournaments.R
+++ tests/testthat/test_get_tournaments.R
@@ -1,5 +1,4 @@
 context("Tournaments")
-library(squashinformr)
 
 test_that("test get_tournaments for wrong input errors", {
 
@@ -7,16 +6,16 @@ test_that("test get_tournaments for wrong input errors", {
   testthat::skip_on_cran()
 
   ## tournament is character
-  expect_error(get_tournaments(year = "2020", world_tour = TRUE))
+  expect_error(squashinformr::get_tournaments(year = "2020", world_tour = TRUE))
 
   ## world_tour is not logical
-  expect_error(get_tournaments(year = 2020, world_tour = "TRUE"))
+  expect_error(squashinformr::get_tournaments(year = 2020, world_tour = "TRUE"))
 
   ## year does not have 4 digits
-  expect_error(get_tournaments(year = 20, world_tour = TRUE))
+  expect_error(squashinformr::get_tournaments(year = 20, world_tour = TRUE))
 
   ## year is negative
-  expect_error(get_tournaments(year = -2020, world_tour = TRUE))
+  expect_error(squashinformr::get_tournaments(year = -2020, world_tour = TRUE))
 
 })
 
@@ -25,14 +24,14 @@ test_that("test get_tournaments for proper outputs", {
   testthat::skip_if_offline()
   testthat::skip_on_cran()
 
-  df <- get_tournaments(year = 2021, world_tour = FALSE)
+  df <- squashinformr::get_tournaments(year = 2021, world_tour = FALSE)
   expect_is(df, "data.frame")
   expect_is(df, "tbl")
   expect_equal(length(unique(na.omit(df$category))), 2)
   expect_is(sample(df$date, 1), "Date")
   expect_equal(year(sample(df$date, 1)), 2021)
 
-  df <- get_tournaments(year = 2020, world_tour = TRUE)
+  df <- squashinformr::get_tournaments(year = 2020, world_tour = TRUE)
   expect_is(df, "data.frame")
   expect_is(df, "tbl")
   expect_equal(length(unique(na.omit(df$category))), 2)
